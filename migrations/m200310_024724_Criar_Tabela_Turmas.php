a133e59d663a2787cf2a7726cf837bafbfdb4d8c
junio Hyago <juniohyago@gmail.com>
2020-03-09 23:53:26 -0300

    migration Criar_Tabela_Turmas

---
+++ migrations/m200310_024724_Criar_Tabela_Turmas.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200310_024724_Criar_Tabela_Turmas
+ */
+class m200310_024724_Criar_Tabela_Turmas extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('turmas',
+            [
+                'id'=>$this->primaryKey(),
+                'descricao'=>$this->string()->notNull(),
+                'periodo'=>$this->string()->notNull(),
+                'fk_curso_id'=>$this->integer()->notNull(),
+
+            ]
+
+        );
+        $this->addForeignKey
+        (
+            'fk_curso_id',
+            'turmas',
+            'fk_curso_id',
+            'curso',
+            'id',
+            'CASCADE'
+        );
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200310_024724_Criar_Tabela_Turmas cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200310_024724_Criar_Tabela_Turmas cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}

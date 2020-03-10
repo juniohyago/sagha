82a42a3175bdecaee2688e1cb002a642f31c4c33
junio Hyago <juniohyago@gmail.com>
2020-03-09 22:38:01 -0300

    migration Criar_tabela_Datas_Professor

---
+++ migrations/m200310_013346_Criar_Tabela_Datas_Professor.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200310_013346_Criar_Tabela_Datas_Professor
+ */
+class m200310_013346_Criar_Tabela_Datas_Professor extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('datas_professor',
+            [
+                'id'=>$this->primaryKey(),
+                'dataHora_inicio'=>$this->dateTime()->notNull(),
+                'dataHora_fim'=>$this->dateTime()->notNull(),
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200310_013346_Criar_Tabela_Datas_Professor cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200310_013346_Criar_Tabela_Datas_Professor cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}

85c787f8f40711f819c9ed2743a00b3594cc8488
alexandremanso <alexandre_manso01@hotmail.com>
2020-03-30 18:29:55 -0300

    Alterações em leyout na pagina HOME atualizado

--- views/site/index.php
+++ views/site/index.php
@@ -8,25 +8,26 @@ $this->title = 'SAGHA';
 
     <div class="jumbotron">
         <h1>SAGHA</h1>
+        <img src="../../web/img/logo.jpeg">
 
-        <p class="lead">SISTEMA DE APOIO E GERENCIAMENTO DE HORÁRIOS ACADÊMICOS.</p>
+        <p class="lead">Sistema de apoio e gerenciamento de horários acadêmicos.</p>
 
         <p><a class="btn btn-lg btn-success" href="site\login">Faça o login para começar.</a></p>
     </div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>História</h2>
-
-                <p>Projeto aprovado como requisito parcial para a obtenção do título de Tecnólogo em
-                    Análise e Desenvolvimento de Sistemas, no Curso Superior de Tecnologia em
-                    Análise e Desenvolvimento de Sistemas da Faculdade Estácio de Sá Goiás –
-                    FESGO.</p>
-
+<!--    <div class="body-content">-->
+<!---->
+<!--        <div class="row">-->
+<!--            <div class="col-lg-4">-->
+<!--                <h2>História</h2>-->
+<!---->
+<!--                <p>Projeto aprovado como requisito parcial para a obtenção do título de Tecnólogo em-->
+<!--                    Análise e Desenvolvimento de Sistemas, no Curso Superior de Tecnologia em-->
+<!--                    Análise e Desenvolvimento de Sistemas da Faculdade Estácio de Sá Goiás –-->
+<!--                    FESGO.</p>-->
+<!---->
 <!--                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>-->
-            </div>
+<!--            </div>-->
 <!--            <div class="col-lg-4">-->
 <!--                <h2>Heading</h2>-->
 <!---->
@@ -48,6 +49,6 @@ $this->title = 'SAGHA';
 <!--                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>-->
 <!--            </div>-->
 <!--        </div>-->
-
-    </div>
+<!---->
+<!--    </div>-->
 </div>

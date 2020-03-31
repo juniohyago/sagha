3a5d682bde3bbb433c3a096459a0e149a4527919
alexandremanso <alexandre_manso01@hotmail.com>
2020-03-30 22:02:19 -0300

    Alterações em leyout - logo

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => 'SAGHA',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -38,9 +38,9 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Pagina inicial', 'url' => ['/site/index']],
+//            ['label' => 'Quem somos', 'url' => ['/site/about']],
+            ['label' => 'Contato', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
@@ -71,8 +71,8 @@ AppAsset::register($this);
     <div class="container">
         <p class="pull-left">&copy; SAGHA <?= date('Y') ?></p>
 
-        <p class="pull-right">Criado por: Alexandre Fernandes Manso;
-                                          Junio Hyago Jorge da Mata;
+        <p class="pull-right">Criado por: Alexandre Fernandes Manso |
+                                          Junio Hyago Jorge da Mata |
                                           Thulio Thalles Amaral Carneiro.
         </p>
     </div>
--- views/site/index.php
+++ views/site/index.php
@@ -3,52 +3,16 @@
 /* @var $this yii\web\View */
 
 $this->title = 'SAGHA';
+
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>SAGHA</h1>
-        <img src="../../web/img/logo.jpeg">
+        <img src="<?=Yii::getAlias('@web').'/img/logosite.png'?>">
 
         <p class="lead">Sistema de apoio e gerenciamento de horários acadêmicos.</p>
 
         <p><a class="btn btn-lg btn-success" href="site\login">Faça o login para começar.</a></p>
     </div>
 
-<!--    <div class="body-content">-->
-<!---->
-<!--        <div class="row">-->
-<!--            <div class="col-lg-4">-->
-<!--                <h2>História</h2>-->
-<!---->
-<!--                <p>Projeto aprovado como requisito parcial para a obtenção do título de Tecnólogo em-->
-<!--                    Análise e Desenvolvimento de Sistemas, no Curso Superior de Tecnologia em-->
-<!--                    Análise e Desenvolvimento de Sistemas da Faculdade Estácio de Sá Goiás –-->
-<!--                    FESGO.</p>-->
-<!---->
-<!--                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>-->
-<!--            </div>-->
-<!--            <div class="col-lg-4">-->
-<!--                <h2>Heading</h2>-->
-<!---->
-<!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et-->
-<!--                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip-->
-<!--                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu-->
-<!--                    fugiat nulla pariatur.</p>-->
-<!---->
-<!--                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>-->
-<!--            </div>-->
-<!--            <div class="col-lg-4">-->
-<!--                <h2>Heading</h2>-->
-<!---->
-<!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et-->
-<!--                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip-->
-<!--                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu-->
-<!--                    fugiat nulla pariatur.</p>-->
-<!---->
-<!--                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--    </div>-->
 </div>
